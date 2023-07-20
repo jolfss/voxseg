9e2cd1cdee488d1441c7a38adc8abdc74f8539f2
Sean Brynjolfsson <smb459@cornell.edu>
2023-07-20 10:30:22 -0400

    Heavy commit. Implemented classes/colors and labels. Converted backend of voxels.py to Torch and reimplemented everything this way--hopefuly parallelization can speed things up soon.

--- exts/omni.voxseg/omni/voxseg/extension.py
+++ exts/omni.voxseg/omni/voxseg/extension.py
@@ -1,5 +1,11 @@
+from typing import Dict, Tuple
+import numpy as np
+import torch
+from torch import Tensor
+
 import omni.ext
 import omni.ui as ui
+from omni.ui import AbstractValueModel, AbstractItemModel
 
 from .voxels import Voxels
 
@@ -9,12 +15,9 @@ TEXT_PAD = ' '*int(PAD/5)
 
 class MyExtension(omni.ext.IExt):
     """The extension object for VoxSeg."""
-    voxels : Voxels
-    class_labels:dict = {} # TODO: If it turns out grouping labels is important, make this a dictionary that maps key (class label) --> value (grouped labels)
 
-    # Dynamic ui elements
-    classes_string_field : ui.StringField
-    classes_vstack : ui.VStack
+    voxels : Voxels
+    "The container object for voxel data."
 
     def build_extension(self) -> ui.Window:
         """Builds the ui elements of the Voxseg Extension."""
@@ -23,60 +26,124 @@ class MyExtension(omni.ext.IExt):
             with ui.ScrollingFrame():
                 with ui.VStack(height=0.0,spacing=PAD):
                     self.build_class_label_editor()
-                    self.build_class_labels_vstack()
+                    self.build_class_vstack()
                     self.build_voxseg_tools()
                     self.build_visualization_tools()
         return window
+    
+    #-------------------------#
+    #   class labels widgets  #
+    #-------------------------#
+    """The fundamental idea is that each color represents a class, so instead of making structure which groups labels
+    under a particular class, simply group all labels by the color they were defined with."""
+
+    color_to_sublabels_dict : dict = {}
+    """Groups sublabels by their color, their de facto class. (r,g,b) (float * float * float) -> labels (str list)
+    NOTE: The key can be any valid argument to ui.color(), which has many overloads."""
+
+    label_to_color_dict : dict = {}
+    "label (str) -> class color (int==ui.ColorShade)"
 
     def build_class_label_editor(self):
-        """Creates the first widget group meant for defining class labels.
-        Defines:
-            self.class_label_string_field (ui.StringField):
-            self.class_label_color_widget (ui.ColorWidget):
-        Requires:
-            self.class_labels_vstack (ui.VStack): The VStack which will contain all of the class labels."""
-        def add_label_fn():
+        """Creates the first widget group meant for defining class labels."""
+
+        def get_current_color_and_label() -> Tuple[Tuple[float,float,float],str]:
+            """reads in the label and color in the ui and returns them"""
             user_input = self.class_label_string_field.model.as_string.strip()
-            color_model  = self.class_label_color_widget.model
-            color_list=[(color_model.get_item_value_model(child)).as_float for child in color_model.get_item_children()[:3]]
-            color = ui.color(color_list[0],color_list[1],color_list[2])
-            if not user_input in self.class_labels.keys():
-                self.class_labels.update({user_input:color}) # TODO: Determine type of sublabels.
-                self.update_class_labels_vstack()
+            color_model  = self.class_color_widget.model
+            r,g,b=[(color_model.get_item_value_model(child)).as_float for child in color_model.get_item_children()[:3]]
+            return (r,g,b), user_input
+
+        def create_new_class():
+            """Changes the color (creates a new class)."""
+            (r,g,b), _ = get_current_color_and_label()      
+
+            # make random colors until one is not in the current list of colors
+            while (r,g,b) in self.color_to_sublabels_dict.keys():
+                r,g,b = np.random.rand(3)
+
+            # removes all colors with no sublabels, NOTE: Not the best way to do this but usually there are few colors
+            for color in list(self.color_to_sublabels_dict.keys()):
+                if len(self.color_to_sublabels_dict[color]) == 0:
+                    self.color_to_sublabels_dict.pop(color) 
+
+            # create new color with no labels
+            self.color_to_sublabels_dict.update({(r,g,b):[]})
+
+            # change the color-picker widget to match the generated color
+            widget = self.class_color_widget.model
+            rmodel,gmodel,bmodel=[(widget.get_item_value_model(child)) for child in widget.get_item_children()[:3]]
+            rmodel.set_value(r)
+            gmodel.set_value(g)
+            bmodel.set_value(b)
+            
+            self.update_class_vstack()
+
+        def create_new_sublabel(required_for_end_edit):
+            """Adds a new label for segmentation to the current color (class)."""
+            (r,g,b), label = get_current_color_and_label()
+
+            if len(label) < 1: # one character is already borderline nonsense, but as long as there's something...
+                return
+            
+            if label in self.label_to_color_dict.keys(): 
+                print(F"[voxseg] Warning: Label {label} is already reserved.")
+                return
+
+            if not (r,g,b) in self.color_to_sublabels_dict.keys(): # handle case where label is added before class
+                self.color_to_sublabels_dict.update({(r,g,b):[]})
+            
+            self.label_to_color_dict.update({label:(r,g,b)}) 
+
+            sublabels : list = self.color_to_sublabels_dict[(r,g,b)] 
+            sublabels.append(label)
+
+            self.update_class_vstack()
 
         with ui.CollapsableFrame("Class Labels"):
+            STARTING_COLOR = 0.8,0.2,0.1
             with ui.VStack(height=0):
-                ui.Button("add label", clicked_fn=add_label_fn)
                 with ui.HStack():
-                    self.class_label_string_field = ui.StringField()
-                    self.class_label_color_widget = ui.ColorWidget()
+                    ui.Button("Change Color (New Class)", clicked_fn=create_new_class)
+                    self.class_color_widget = ui.ColorWidget(*STARTING_COLOR)
+                    ui.AbstractItemModel.add_end_edit_fn(self.class_color_widget.model, create_new_class)
 
-    def build_class_labels_vstack(self):
-        """Creates the widget which will contain all of the defined class labels and a button to clear them.
-        Defines:
-            class_labels_vstack (ui.VStack): """
+                self.button_assign_new_sublabel = ui.Button("Assign New Sublabel", clicked_fn=create_new_sublabel)
+                self.class_label_string_field = ui.StringField()
+
+                # NOTE: Preferable to button if there is a way to trigger on pressing enter, deselecting is annoying.
+                #ui.AbstractValueModel.add_end_edit_fn(self.class_label_string_field.model, create_new_sublabel)
+                
+
+    def build_class_vstack(self):
+        """Contains all of the registered labels, has a button to clear them."""
+
         def clear_labels_fn():
-            self.class_labels.clear()
-            self.update_class_labels_vstack()
+            """Empties the current list of labels and colors (classes) and reloads their container."""
+            self.color_to_sublabels_dict.clear()
+            self.label_to_color_dict.clear()
+            self.update_class_vstack()
 
         with ui.CollapsableFrame("View Labels"):
-            self.class_labels_vstack = ui.VStack(height=0, spacing=PAD)
-        ui.Button("Clear Labels", clicked_fn=clear_labels_fn)
+            with ui.VStack():
+                self.class_vstack = ui.VStack(height=0, spacing=PAD)
+                ui.Button("Clear Labels", clicked_fn=clear_labels_fn)
 
-    def update_class_labels_vstack(self):
-        """Empties the current vstack of all class labels and rebuilds them from [self.class_labels]
-        Requires:
-            self.class_labels_vstack (ui.VStack)"""
-        print("Update called")
-        self.class_labels_vstack.clear()
-        with self.class_labels_vstack:
-            for class_label in self.class_labels.keys():
-                ui.Label(class_label, word_wrap=True, style={"color":self.class_labels[class_label]})
+    def update_class_vstack(self):
+        """Clears and then rebuilds the vstack with all colors (classes) and their labels."""
+        self.class_vstack.clear()
+        with self.class_vstack:
+            for class_color in self.color_to_sublabels_dict.keys():
+                for sublabel in self.color_to_sublabels_dict[class_color]:
+                    ui.Label(sublabel, style={"font_size": 40.0, "color":ui.color(*class_color)})
 
+    #-------------------------#
+    #   voxseg-tools widgets  #
+    #-------------------------#
+    
+    # TODO: Set up editing.
     def build_voxseg_tools(self):
-        """Creates the widget which will contain all of the defined class labels.
-        Defines:
-            class_labels_vstack (ui.VStack): """
+        """Creates the widget which will set voxseg parameters."""
         with ui.CollapsableFrame("Voxseg Parameters"):
             with ui.VStack(height=0,spacing=PAD):
                 with ui.HStack():
@@ -84,11 +151,29 @@ class MyExtension(omni.ext.IExt):
                     ui.MultiFloatDragField(0.0,0.0,0.0,width=ui.Fraction(3))
                 with ui.HStack():
                     ui.Label(F"{TEXT_PAD}World Dims{TEXT_PAD}",width=ui.Fraction(1))
-                    ui.MultiFloatDragField(1.0,1.0,1.0,min=1.0,step=0.1,width=ui.Fraction(3))
+                    ui.MultiFloatDragField(20.0,20.0,20.0,min=1.0,step=0.1,width=ui.Fraction(3))
                 with ui.HStack():
                     ui.Label(F"{TEXT_PAD}Grid Dims{TEXT_PAD}",width=ui.Fraction(1))
-                    ui.MultiIntDragField(2,2,2,min=2,width=ui.Fraction(3))
-                    
+                    ui.MultiIntDragField(10,10,10,min=2,width=ui.Fraction(3))
+
+    #---------------------------------#
+    #   voxseg-visualization widgets  #
+    #---------------------------------#
+
+    def __debug_create_all_voxels(self):
+        # self.voxels.create_voxel_prims(
+        #     torch.arange(min(self.voxels.grid_dims)).expand(3,min(self.voxels.grid_dims)).T.clone())
+        gx,gy,gz = self.voxels.grid_dims
+        GX, GY, GZ = torch.arange(gx),torch.arange(gy),torch.arange(gz)
+        GXE, GYE, GZE = GX.expand(gz,gy,-1), GY.expand(gx,gz,-1), GZ.expand(gx,gy,-1)
+        GXEP, GYEP = GXE.permute(2,1,0), GYE.permute(0,2,1)
+        all_voxels = torch.stack((GXEP,GYEP,GZE),dim=3).view(-1,3)
+        self.voxels.create_voxel_prims(all_voxels)
+
+    def visualize_occupancy_fn(self):
+        
+
+
     def build_visualization_tools(self):
         """TODO: Docs"""
         with ui.CollapsableFrame("Voxel Visualization"):
@@ -96,11 +181,10 @@ class MyExtension(omni.ext.IExt):
                 with ui.VStack():
                     ui.Label(F"{TEXT_PAD}Total Occupied Voxels: <UNIMPLEMENTED>")
                     ui.Label(F"{TEXT_PAD}Number of Photos: <UNIMPLEMENTED>")
-                ui.Button("visualize occupancy")
+                ui.Button("visualize occupancy", clicked_fn=self.visualize_occupancy_fn) #TODO:
                 ui.Button("segment over labels")
                 ui.Button("clear segments")
                 ui.Button("hide/show voxels")
-
 
     def on_startup(self, ext_id):
 
--- exts/omni.voxseg/omni/voxseg/voxels.py
+++ exts/omni.voxseg/omni/voxseg/voxels.py
@@ -1,127 +1,160 @@
-from typing import Callable, List, Sequence, Tuple
+from typing import Callable, Dict, List, Sequence, Tuple
 import torch
 from torch import Tensor
 import pxr
 from pxr import Gf, UsdGeom, Sdf
 import omni
-import numpy as np
 
 class Voxels():
-    """Visualizes voxels."""
-    grid_dims       : Tuple[int,int,int]        
-    "(gx, gy, gz) The grid dimensions."
-    full_grid_dims  : Tuple[int,int,int]        
-    "(gx+2,gy+2,gz+2) The grid dimensions including the buffer shell."
-    world_dims      : Tuple[float,float,float]
-    "(wx, wy, wz) The world dimensions of the voxel space, not including the buffer shell."
+    """Visualizes voxels.
+    TODO: Docs
+    """
 
-    G : Tensor 
-    "(3,)=[gx, gy, gz] The grid dimensions (how many voxels along each axis)."
+    voxel_prim_directory : str
+    "The location of the voxel prims. Default: /World/VOXSEG/voxels"
+
+    G   : Tensor 
+    "(3,) The grid dimensions (how many voxels along each axis)."
+    grid_dims       : Tuple[int,int,int]        
+    "(gx, gy, gz)   The grid dimensions."
+
     _G_ : Tensor 
-    "(3,)=[gx+2,gy+2,gz+2] The grid dimensions *including* the buffer shell. The underscores denote the buffer."
-    W : Tensor 
-    "(3,)=[wx, wy, wz] The world dimensions of the voxel space, not including the buffer shell."
+    "(3,) The grid dimensions *including* the buffer shell (+2 each dimension). The underscores denote the buffer."
+    _grid_dims_  : Tuple[int,int,int]        
+    "(gx+2,gy+2,gz+2)   The grid dimensions including the buffer shell."
+
+    world_dims      : Tuple[float,float,float]
+    "(wx, wy, wz)   The world dimensions of the voxel space, not including the buffer shell."
+    W   : Tensor 
+    "(3,) The world dimensions of the voxel space, not including the buffer shell."
 
     __voxel_indices : Tensor
-    "(gx,gy,gz,:)=[i,j,k] At each i,j,k, a tensor corresponding to that voxel index."
+    "(gx,gy,gz,:) At each i,j,k, a tensor corresponding to that voxel index."
     __voxel_centers : Tensor
-    "(gx,gy,gz,:)=[wx,wy,wz] At each i,j,k, a tensor corresponding to the center of voxel_ijk."
-    voxels        : dict 
-    "Maps voxel index (i,j,k) to the primpath of the voxel prim."
-    num_voxels    : int
-    "Number of total possible voxels in the voxel grid."
+    "(gx,gy,gz,:) At each i,j,k, a tensor corresponding to the center in world space of voxel_ijk."
+    __voxel_counts : dict = {}
+    "Tensor([gx,gy,gz]) -> int, At each i,j,k, the number of times the voxel has been observed."
+    __voxel_prims : dict 
+    "Tensor([gx,gy,gz]) -> prim_path_ijk to the primpath of the voxel prim."
 
-    def __init__(self, world_dims, grid_dims, device='cuda'):
+    def __init__(self, world_dims, grid_dims, device='cpu', voxel_prim_directory="/World/VOXSEG/voxels"):
+        """TODO: Docs"""
+        self.grid_dims      = grid_dims
+        self._grid_dims_    = grid_dims[0]+2, grid_dims[1]+2, grid_dims[2]+2
+        self.world_dims     = world_dims
 
-        self.grid_dims = grid_dims
-        self.full_grid_dims = (grid_dims[0]+2, grid_dims[1]+2, grid_dims[2]+2)
-        self.world_dims = world_dims
+        self.device         = device
+        self.voxel_prim_directory = voxel_prim_directory
 
-        G = self.G = torch.tensor([grid_dims[0], grid_dims[1], grid_dims[2]], device=device)
-        W = self.W = torch.tensor([world_dims[0],world_dims[1],world_dims[2]], device=device)
-        # Includes buffer, a layer of voxels along each boundary. NOTE: Underscores illustrate the boundary _GRID_.
-        _G_ = self._G_ = torch.tensor([G[0]+2, G[1]+2, G[2]+2], device=device) 
-        X = torch.arange(_G_[0], device=device).view(-1, 1, 1).expand(-1,      _G_[1], _G_[2])
-        Y = torch.arange(_G_[1], device=device).view(1, -1, 1).expand(_G_[0],  -1,     _G_[2])
-        Z = torch.arange(_G_[2], device=device).view(1, 1, -1).expand(_G_[0],  _G_[1], -1    )
+        G = self.G = torch.tensor([grid_dims[0], grid_dims[1], grid_dims[2]], device=self.device)
+        W = self.W = torch.tensor([world_dims[0],world_dims[1],world_dims[2]], device=self.device)
+
+        # Create buffer, a voxel shell capping each dimension. Underscores are intended to denote the boundary _GRID_.
+        _G_ = self._G_ = torch.tensor([G[0]+2, G[1]+2, G[2]+2], device=self.device) 
+        X   = torch.arange(_G_[0], device=self.device).view(-1, 1, 1).expand(-1,    _G_[1],_G_[2])
+        Y   = torch.arange(_G_[1], device=self.device).view(1, -1, 1).expand(_G_[0],-1,    _G_[2])
+        Z   = torch.arange(_G_[2], device=self.device).view(1, 1, -1).expand(_G_[0],_G_[1],-1    )
 
         # Shifts everything down one in all dimensions to make the buffer.
         self.__voxel_indices = torch.stack((X,Y,Z), dim=-1) - 1 
         self.__voxel_centers = (1/G * (self.__voxel_indices - (G-1)/2)) * W
+        self.__voxel_prims   = {}
+        self.num_voxels      = _G_[0]*_G_[1]*_G_[2]
 
-        self.voxels = {}
-        self.num_voxels = _G_[0]*_G_[1]*_G_[2]
-
-    def get_voxel_index(self, voxel_index_ijk : Tuple[int,int,int]) -> Tuple[int,int,int]:
-        """The index to find the i,j,k-th voxel at.
-        
-        Note:
-            indices[0...G-1] represents the main storage area
-            indices[-1,G] represent the buffer"""
-        i,j,k = voxel_index_ijk
-        return (i+1,j+1,k+1)
+    def capacity(self, include_buffer=False) -> int:
+        "Number of total possible voxels in the voxel grid."
+        roving_product = 1
+        for dim in (self._G_ if include_buffer else self.G):
+            roving_product *= dim
+        return roving_product
     
-    def get_voxel_center(self, voxel_index_ijk : Tuple[int,int,int]) -> Tensor:
-        """The center of the i,j,k-th voxel.
+    def get_voxel_centers(self, voxel_indices : Tensor) -> Tensor:
+        """Is a (N,3) tensor of the centers of all voxels in [voxel_indices].
+
+        Args:
+            voxel_indices (N,3): Stack of voxel indices of voxel to get the center of.
 
         Requires:
-            i,j,k \in [-1...G] inclusive (per dimension of G)"""
-        i,j,k = voxel_index_ijk
-        return self.__voxel_centers[i+1,j+1,k+1]
+            Forall n, voxel_indices[n,:] \in [-1...G] inclusive (per dimension of G)"""
+        voxel_indices += 1
+        return self.__voxel_centers[voxel_indices[:, 0], voxel_indices[:, 1], voxel_indices[:, 2], :]
 
-    #NOTE: May be deprecated in favor of methods which just spawn via cube and transform.
-    def create_voxel_prim(self, voxel_index_ijk : Tuple[int,int,int], device='cuda') -> str:  
-        """Creates the voxel for the i,j,k-th voxel in the stage, or does nothing if it already exists."""
-        i,j,k = voxel_index_ijk[:]
-        i,j,k = i+1,j+1,k+1
+    #NOTE: May be soon deprecated if I find a way to make an instancer.
+    def create_voxel_prims(self, voxel_indices : Tensor):  
+        """Creates the voxel for the i,j,k-th voxel in the stage, or does nothing if it already exists.
+        Args:
+            voxel_indices (N,3): Stack of row vectors [i,j,k] denoting which voxels to create."""
+
+        def Gf_Vec3f_list_list_of_vertices_tensor(vertices_tensor : Tensor) -> List[List[Gf.Vec3f]]:
+            """Converts a tensor of vertices (N,8,3) in a torch tensor to a Gf.Vec3f list list."""
+            (N,_,__) = vertices_tensor.size()
+            gf_vec3f_list_list = []
+            for n in range(N):
+                gf_vec3f_list = []
+                for v in range(8):
+                    gf_vec3f_list.append(Gf.Vec3f(float(vertices_tensor[n,v,0].item()),
+                                                  float(vertices_tensor[n,v,1].item()),
+                                                  float(vertices_tensor[n,v,2].item())))
+                gf_vec3f_list_list.append(gf_vec3f_list)
+            return gf_vec3f_list_list
         
-        voxel_prim_path = F"/World/Voxels/voxel_{i}_{j}_{k}"
-        if voxel_index_ijk in self.voxels:
-            apply_color_to_prim((np.random.random(),np.random.random(),np.random.random()))(voxel_prim_path)
-            return voxel_prim_path
-        
-        self.voxels[voxel_index_ijk]=voxel_prim_path
-        
-        def Gf_Vec3f_list_of_vertices_tensor(vertices_tensor) -> List[Gf.Vec3f]:
-            """Converts a tensor of vertices in a torch tensor to a list of Gf.Vec3f tensors."""
-            gfvec3f_list = []
-            for vertex in vertices_tensor:
-                gfvec3f_list.append(Gf.Vec3f(float(vertex[0]), float(vertex[1]), float(vertex[2])))
-            return gfvec3f_list
-        
-        def get_vertices_of_center(center : Tensor) -> Tensor: 
-            """Is the set of vertices which make up the voxel for the voxel at [voxel_index_ijk]."""
+        def get_vertices_of_center(centers : Tensor) -> Tensor: 
+            """Is the (N,8,3) tensor containing the vertices for all centers, satisfying the below convention."""
             """ 2---4                    5-------4      
                 | \ |                   /|      /|      Triangle Faces (12)
             2---3---5---4---2          7-|-----6 |      342 354 375 307 310 321
             | \ | / | \ | / |   -->    | 3-----|-2      764 745 701 716 146 124
             1---0---7---6---1          |/      |/       
-                | \ |                  0-------1        NOTE: These faces have outwards normal
+                | \ |                  0-------1        NOTE: These faces have outwards normal.
                 1---6"""  
             unit_box = torch.tensor([(-1,-1,-1),(1,-1,-1),(1,1,-1),
-                                     (-1,1,-1),(1,1,1),(-1,1,1),(1,-1,1),(-1,-1,1)],device=device)
-            return (unit_box*self.W)/(2*self.G) + center
+                                     (-1,1,-1),(1,1,1),(-1,1,1),(1,-1,1),(-1,-1,1)],device=self.device)
+            
+            unit_box = unit_box.view(8, 1, 3)
+            
+            centers = centers.view(1, -1, 3)
+            
+            vertices = (unit_box * self.W) / (2 * self.G) + centers
+            
+            # Reshape to (N, 8, 3) for convenience
+            vertices = vertices.transpose(0, 1)
+            return vertices
         
+        voxel_indices += 1
+        (N,_) = voxel_indices.size()
+
+        voxel_centers = self.get_voxel_centers(voxel_indices)
+        gf_vec3f_list_list = Gf_Vec3f_list_list_of_vertices_tensor(get_vertices_of_center(voxel_centers))
+
+        for n in range(N):
+            voxel_prim_path = \
+            F"{self.voxel_prim_directory}/voxel_{voxel_indices[n][0]}_{voxel_indices[n][1]}_{voxel_indices[n][2]}"
+
+            self.__voxel_prims[voxel_indices[n]] = voxel_prim_path   
+
+            mesh = UsdGeom.Mesh.Define(omni.usd.get_context().get_stage(), voxel_prim_path)
+            mesh.CreatePointsAttr(gf_vec3f_list_list[n])
+            mesh.CreateFaceVertexCountsAttr(12*[3]) # 12 tris per cube, 3 vertices each
+            mesh.CreateFaceVertexIndicesAttr([3,4,2,  3,5,4,  3,7,5,  3,0,7,  3,1,0,  3,2,1,
+                                            7,6,4,  7,4,5,  7,0,1,  7,1,6,  1,4,6,  1,2,4,])    
         
-        mesh = UsdGeom.Mesh.Define(omni.usd.get_context().get_stage(), voxel_prim_path)
-        mesh.CreatePointsAttr(Gf_Vec3f_list_of_vertices_tensor(get_vertices_of_center(self.__voxel_centers[i,j,k])))
-        mesh.CreateFaceVertexCountsAttr(12*[3]) # 12 tris per cube, 3 vertices each
-        mesh.CreateFaceVertexIndicesAttr([3,4,2,  3,5,4,  3,7,5,  3,0,7,  3,1,0,  3,2,1,
-                                          7,6,4,  7,4,5,  7,0,1,  7,1,6,  1,4,6,  1,2,4,])
-        
-        apply_color_to_prim((np.random.random(),np.random.random(),np.random.random()))(voxel_prim_path)        
-                                          
-        return voxel_prim_path
+    def receive_voxel_counts(self, tensor_ijk_count : Tensor):
+        """Updates/populates [self.__voxel_counts] to have the count numbers for each voxel_ijk in [tensor_ijk_count].
+        Args:
+            tensor_ijk_count (N,4): Each row is of the form [i,j,k,count]."""
+        (N,_) = tensor_ijk_count.size()
+        for n in range(N):
+            self.__voxel_counts.update({tensor_ijk_count[n,:3],tensor_ijk_count[3]})
 
 def apply_color_to_prim(color: tuple) -> Callable[[str],None]:
-    def __apply_color_to_prim_helper(prim_path : str) -> Tuple[float,float,float] :
-        """
-        Apply an RGB color to a prim. 
+    """Apply an RGB color to a prim. 
 
-        Parameters:
-        prim_path (str): The path to the prim.
+    Args:
         color (tuple): The RGB color to apply as a tuple of three floats.
-        """
+    
+    Lambda Args:
+        prim_path (str): The path to the prim."""
+    def __apply_color_to_prim_helper(prim_path : str) -> Tuple[float,float,float] :
         stage = omni.usd.get_context().get_stage()
 
         # Access the prim
@@ -143,3 +176,44 @@ def apply_color_to_prim(color: tuple) -> Callable[[str],None]:
         else:
             prim_geom.GetDisplayColorAttr().Set([Gf.Vec3f(*color)])
     return __apply_color_to_prim_helper
+
+
+    # DEPRECATED FOR MULTIPLICITY VERSION
+    # #NOTE: May be deprecated in favor of methods which just spawn via cube and transform.
+    # def create_voxel_prim(self, voxel_index_ijk : Tuple[int,int,int]) -> str:  
+    #     """Creates the voxel for the i,j,k-th voxel in the stage, or does nothing if it already exists."""
+    #     i,j,k = voxel_index_ijk[:]
+    #     i,j,k = i+1,j+1,k+1
+        
+    #     voxel_prim_path = F"{self.voxel_prim_directory}/voxel_{i}_{j}_{k}"
+
+    #     self.__voxel_prims[voxel_index_ijk]=voxel_prim_path
+        
+    #     def Gf_Vec3f_list_of_vertices_tensor(vertices_tensor) -> List[Gf.Vec3f]:
+    #         """Converts a tensor of vertices in a torch tensor to a list of Gf.Vec3f tensors."""
+    #         gfvec3f_list = []
+    #         for vertex in vertices_tensor:
+    #             gfvec3f_list.append(Gf.Vec3f(float(vertex[0]), float(vertex[1]), float(vertex[2])))
+    #         return gfvec3f_list
+        
+    #     def get_vertices_of_center(center : Tensor) -> Tensor: 
+    #         """Is the set of vertices which make up the voxel for the voxel at [voxel_index_ijk]."""
+    #         """ 2---4                    5-------4      
+    #             | \ |                   /|      /|      Triangle Faces (12)
+    #         2---3---5---4---2          7-|-----6 |      342 354 375 307 310 321
+    #         | \ | / | \ | / |   -->    | 3-----|-2      764 745 701 716 146 124
+    #         1---0---7---6---1          |/      |/       
+    #             | \ |                  0-------1        NOTE: These faces have outwards normal
+    #             1---6"""  
+    #         unit_box = torch.tensor([(-1,-1,-1),(1,-1,-1),(1,1,-1),
+    #                                  (-1,1,-1),(1,1,1),(-1,1,1),(1,-1,1),(-1,-1,1)],device=self.device)
+    #         return (unit_box*self.W)/(2*self.G) + center
+        
+        
+    #     mesh = UsdGeom.Mesh.Define(omni.usd.get_context().get_stage(), voxel_prim_path)
+    #     mesh.CreatePointsAttr(Gf_Vec3f_list_of_vertices_tensor(get_vertices_of_center(self.__voxel_centers[i,j,k])))
+    #     mesh.CreateFaceVertexCountsAttr(12*[3]) # 12 tris per cube, 3 vertices each
+    #     mesh.CreateFaceVertexIndicesAttr([3,4,2,  3,5,4,  3,7,5,  3,0,7,  3,1,0,  3,2,1,
+    #                                       7,6,4,  7,4,5,  7,0,1,  7,1,6,  1,4,6,  1,2,4,])     
+                                          
+    #     return voxel_prim_path
